0ec342e8b4c7efbec1b5b112f24307ea01887fa4
riza <rizamili3@gmail.com>
2020-09-12 12:26:54 +0430

    Implement views

---
+++ BasicEncryptionApp/Modules/Detail/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  BasicEncryptionApp
+//
+//  Created by riza milani on 6/22/1399 AP.
+//  Copyright © 1399 reza milani. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
