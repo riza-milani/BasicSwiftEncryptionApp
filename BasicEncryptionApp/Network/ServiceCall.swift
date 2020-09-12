716a70274a6f9177db54fcc9a09e0350c2200536
riza <rizamili3@gmail.com>
2020-09-12 22:37:39 +0430

    improve network to genetic and mock service call

--- BasicEncryptionApp/Network/ServiceCall.swift
+++ BasicEncryptionApp/Network/ServiceCall.swift
@@ -12,11 +12,11 @@ typealias ResponseData = String
 let baseUrl = "http://service-address-com/sample.json"
 
 protocol ServiceCall {
-    func fetchData(completion: @escaping ((Result<ResponseData, Error>) -> Void))
+    func fetchData<T: Decodable>(completion: @escaping ((Result<T, Error>) -> Void))
 }
 
 class ServiceCallApi: ServiceCall {
-    func fetchData(completion: @escaping ((Result<ResponseData, Error>) -> Void)) {
+    func fetchData<T: Decodable>(completion: @escaping ((Result<T, Error>) -> Void)) {
         DispatchQueue.global(qos: .background).async {
             let endpoint: String = baseUrl
             guard let url = URL(string: endpoint) else {
@@ -36,13 +36,39 @@ class ServiceCallApi: ServiceCall {
                     completion(.failure(NSError(domain: "There is no data response.", code: 0, userInfo: nil)))
                     return
                 }
-                guard let responseData = try? JSONDecoder().decode(ResponseData.self, from: dataResponse) else {
+                guard let responseData = try? JSONDecoder().decode(T.self, from: dataResponse) else {
                     completion(.failure(NSError(domain: "Can not decode the data.", code: 0, userInfo: nil)))
                     return
                 }
                 completion(.success(responseData))
             }
             task.resume()
+        }
+    }
+}
+
+class ServiceCallLocal: ServiceCall {
+
+    let basicEncryption = BasicEncryption()
+
+    func mockEncryptionData() -> Data? {
+        if let data = "{ \"result\": \"This is simple test\"}".data(using: .utf8) {
+            let result = self.basicEncryption.encrypt(data, withPassword: "12345")
+            return try? JSONEncoder().encode(result)
+        }
+        return nil
+    }
+
+    func fetchData<T: Decodable>(completion: @escaping ((Result<T, Error>) -> Void)) {
+        DispatchQueue.global(qos: .userInitiated).async {
+
+            if let data = self.mockEncryptionData() {
+                guard let responseData = try? JSONDecoder().decode(T.self, from: data) else {
+                    completion(.failure(NSError(domain: "Can not decode the data.", code: 0, userInfo: nil)))
+                    return
+                }
+                completion(.success(responseData))
+            }
         }
     }
 }
