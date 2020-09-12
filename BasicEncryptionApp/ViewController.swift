c541aaaa91fad566156dd5a534e2b05f3a23e3bf
riza <rizamili3@gmail.com>
2020-09-12 11:46:32 +0430

    Initial Commit

---
+++ BasicEncryptionApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BasicEncryptionApp
+//
+//  Created by riza milani on 6/22/1399 AP.
+//  Copyright © 1399 reza milani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BasicEncryptionAppTests/BasicEncryptionAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  BasicEncryptionAppTests.swift
+//  BasicEncryptionAppTests
+//
+//  Created by riza milani on 6/22/1399 AP.
+//  Copyright © 1399 reza milani. All rights reserved.
+//
+
+import XCTest
+@testable import BasicEncryptionApp
+
+class BasicEncryptionAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
